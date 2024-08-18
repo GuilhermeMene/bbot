7c25b3bb773342ff20ce7669ab51dfcccad3cccf
Guilherme Mene <guilherme.ale.primo@live.com>
2024-08-18 12:41:33 +0100

    Update the async functions to run the telegram bot

--- bbot.py
+++ bbot.py
@@ -7,6 +7,7 @@ bbot - A Binance bot for Spot trade BTCUSDT
 import os
 import sys
 import time
+import asyncio
 import pandas as pd
 from bbot import logger as log
 from bbot.client import Client
@@ -204,8 +205,7 @@ if __name__ == '__main__':
 
     #First run the telegram bot
     print("Starting telegram bot...")
-    tproc = Thread(target=telegram_bot.runTelegramBot)
-    tproc.start()
+    asyncio.run(telegram_bot.runTelegramBot)
 
     #Run the bot using 1 minute interval
     while True:
